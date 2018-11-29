64ff7874318a558790ca0ca98af5dfb56a811754
Linda Morales <lindagmorales94@gmail.com>
2018-11-29 09:57:55 -0800

    added some comments about the unicorn model

--- test/models.spec.js
+++ test/models.spec.js
@@ -1,7 +1,7 @@
-const expect = require("chai").expect;
-const { db, Unicorn } = require("../server/unicorn");
+const expect = require('chai').expect;
+const { db, Unicorn } = require('../server/unicorn');
 
-describe("Unicorn", () => {
+describe('Unicorn', () => {
   before(() => {
     return db.sync({ force: true });
   });
@@ -10,43 +10,51 @@ describe("Unicorn", () => {
   });
 });
 
-describe("Model methods", function() {
+/*
+  Your model should have the following fields:
+
+  - name - required
+  - imageUrl - with a default value of http://3.bp.blogspot.com/-kNKu4oTKVJs/TWLJ0Yhw2OI/AAAAAAAAAVo/606P2UAvHj0/s1600/FashionRob5.jpg
+  - age - is an integer
+*/
+
+describe('Model methods', function() {
   beforeEach(async () => {
     return await Promise.all([
       Unicorn.create({
-        name: "Quirin",
+        name: 'Quirin',
         imageUrl:
-          "https://about-mythical-creatures.weebly.com/uploads/9/3/4/7/9347709/7353641_orig.jpg",
-        age: 5000
+          'https://about-mythical-creatures.weebly.com/uploads/9/3/4/7/9347709/7353641_orig.jpg',
+        age: 5000,
       }),
       Unicorn.create({
-        name: "Dabby",
+        name: 'Dabby',
         imageUrl:
-          "https://i.pinimg.com/originals/f4/a0/e8/f4a0e8658deda92fa9b21b003f148853.jpg",
-        age: 8
-      })
+          'https://i.pinimg.com/originals/f4/a0/e8/f4a0e8658deda92fa9b21b003f148853.jpg',
+        age: 8,
+      }),
     ]);
   });
-  describe("Hooks", () => {
-    xit("capitalizes the first letter of the Unicorn name", async () => {
+  describe('Hooks', () => {
+    xit('capitalizes the first letter of the Unicorn name', async () => {
       const rainicorn = await Unicorn.create({
-        name: "lAdY rAinIcOrN",
+        name: 'lAdY rAinIcOrN',
         imageUrl:
-          "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/f/f6/Jake_w_lady.png/revision/latest?cb=20120729200735",
-        age: 18
+          'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/f/f6/Jake_w_lady.png/revision/latest?cb=20120729200735',
+        age: 18,
       });
-      expect(rainicorn.name).to.equal("Lady Rainicorn");
-      await rainicorn.update({name: 'lAdY rAinIcOrN'})
-      expect(rainicorn.name).to.equal("Lady Rainicorn");
+      expect(rainicorn.name).to.equal('Lady Rainicorn');
+      await rainicorn.update({ name: 'lAdY rAinIcOrN' });
+      expect(rainicorn.name).to.equal('Lady Rainicorn');
     });
   });
-  describe("Instance methods", () => {
+  describe('Instance methods', () => {
     let unicorn1;
     beforeEach(async () => {
-      unicorn1 = await Unicorn.create({ name: "Unicorn Friend", age: 18 });
+      unicorn1 = await Unicorn.create({ name: 'Unicorn Friend', age: 18 });
     });
-    describe("Find Birth Year", () => {
-      xit("returns the birth year of a Unicorn", () => {
+    describe('Find Birth Year', () => {
+      xit('returns the birth year of a Unicorn', () => {
         expect(unicorn1.findBirthYear()).to.equal(2000);
       });
     });
