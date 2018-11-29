520924657e473a0115f9598cf79de0a1e9a44f21
Linda Morales <lindagmorales94@gmail.com>
2018-11-29 09:12:14 -0800

    got rid of the react answers

--- src/AllUnicorns.js
+++ src/AllUnicorns.js
@@ -5,42 +5,13 @@ import data from './data';
 import UnicornCard from './UnicornCard';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      unicorns: [],
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ unicorns: data });
-  }
-
   render() {
     return (
       <div className="App container">
         <h1 className="App-intro">Unicorns!!!!!!!</h1>
         <h1 htmlFor="Unicorn">Submit a unicorn!</h1>
-        <form>
-          <div className="form-group">
-            <input name="unicorn" className="form-control" placeholder="Name" />
-            <input
-              name="imageUrl"
-              className="form-control"
-              placeholder="ImageUrl"
-            />
-            <input name="age" className="form-control" placeholder="Age" />
-          </div>
-          <button type="submit" className="btn btn-info">
-            Submit
-          </button>
-        </form>
         <div className="row">
-          {this.state.unicorns.map(unicorn => (
-            <div key={unicorn.id}>
-              <UnicornCard unicorn={unicorn} />
-            </div>
-          ))}
+        {/* Your Code Here */}
         </div>
       </div>
     );
